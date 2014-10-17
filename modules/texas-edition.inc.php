beca027112b076eb757e716507365483355c90fd
Anthony Miller <anthony@tiny-zebra.com>
2014-10-17 15:36:32 -0500

    Added styling to pages

---
+++ modules/texas-edition.inc.php
@@ -0,0 +1,31 @@
+<?php # Script 2.6 - about.inc.php
+if(!defined('BASE_URL')) {
+require('../includes/config.inc.php');
+$url = BASE_URL . 'index.php?p=about';
+header("Location: $url");
+exit;
+} //End if defined() IF.
+?>
+<div class="page-titles">
+	<div class="row">
+		<div class="large-12 columns">
+			<h2>For A Limited Time</h2>
+		</div>
+	</div>
+</div>
+
+<div class="row">
+	<div class="large-12 columns">
+		<h1>The Texas Edition</h1>
+
+	</div>
+</div>
+
+<div class="row">
+	<div class="large-12 columns">
+		<p>This limited edition Texas scooter embodies the Lone Star state spirit with a new emblem, just in time for the 4th
+		 of July.  Show off your patriotism with this luxurious four-wheel scooter and ride around in style. There will be a limited quantity available, so be sure to order yours before they sell out!
+		</p>
+	</div>
+</div>
+
